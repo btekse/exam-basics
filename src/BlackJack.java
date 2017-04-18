28cdd16d3ab5c19d0c63e9b32021badfe70c585c
btekse <btekse@gmail.com>
2017-04-18 11:18:32 +0200

    add(app) : BlackJack v1

---
+++ src/BlackJack.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Balázs on 2017. 04. 18..
+ */
+public class BlackJack {
+  static class Card {
+    String color;
+    int value;
+
+    Card(String color, int value) {
+      this.color = color;
+      this.value = value
+    }
+
+    
+  }
+}
